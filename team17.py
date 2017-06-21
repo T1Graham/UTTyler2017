8dced70d91bbe1f0396f8b50a3bcdf3fc4aa3b04
Tony Graham <tonygraham17@gmail.com>
2017-06-21 15:53:50 -0500

    New Team Logic

--- team17.py
+++ team17.py
@@ -1,3 +1,4 @@
+import random
 ####
 # Each team's file must define four tokens:
 #     team_name: a string
@@ -6,9 +7,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Pikachu' # Only 10 chars displayed.
+strategy_name = 'Wild Guessing'
+strategy_description = 'No Strategy'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -26,8 +27,22 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
-
+    if len(my_history)==0: 
+        return 'c'
+    elif len(their_history)==0:
+        return 'c'
+    elif 'b' in their_history and len(their_history)>100: 
+        return 'b'
+    elif  my_history[-1]=='c' and their_history[-1]=='b':
+            return 'b'
+    elif  my_history[-1]=='' and their_history[-1]=='c':
+            return 'c'
+    elif my_history[-1]=='b' and their_history[-1]=='b':
+            return 'b'
+    elif my_history[-1]=='c' and their_history[-1]=='c':
+            return 'c'              
+    else:
+        return 'c'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
